356e4cf912f9e247c605c3ae90f939420fe7add9
@Mike-Prybytkin <prybytkin@bk.ru>
2022-08-25 23:15:22 +0300

    feat: implement modal window

--- src/view/main-html/main-html.ts
+++ src/view/main-html/main-html.ts
@@ -22,8 +22,7 @@ class ViewHtml {
         <div class="nav-wrapper red lighten-5">
           <a href="#!" class="brand-logo grey-text text-darken-4"><img class="hide-on-small-only school-logo"
               src="./assets/rs-lang-logo.svg" alt="rs lang logo" title="RS Lang">RS-Lang</a>
-          <a class="right deep-orange darken-1 waves-effect waves-light btn login-button"><i
-              class="material-icons left">account_box</i>Log in</a>
+          <a href="#modal-log-in" class="modal-trigger right deep-orange darken-1 waves-effect waves-light btn login-button"><i class="material-icons left">account_box</i>Вход</a>
           <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i
               class="material-icons grey-text text-darken-4">menu</i></a>
           <ul class="nav-items right hide-on-med-and-down">
@@ -44,6 +43,64 @@ class ViewHtml {
         <li><img class="hide-on-med-only mobile-school-logo" src="./assets/rs-lang-logo.svg" alt="rs lang logo"
             title="RS Lang"></a></li>
       </ul>
+
+      <div id="modal-log-in" class="modal">
+        <div class="modal-content">
+          <ul class="tabs center">
+            <li class="tab tab-modal"><a class="active" href="#log-in-swipe">Вход</a></li>
+            <li class="tab tab-modal"><a href="#registration-swipe">Регистрация</a></li>
+          </ul>
+
+          <div id="log-in-swipe" class="center">
+            <div class="input-field">
+              <i class="material-icons prefix">email</i>
+              <input id="email-log-in" type="email" class="validate">
+              <label for="email-log-in">Email</label>
+              <span class="helper-text" data-error="invalid email" data-success="perfectly"></span>
+            </div>
+            <div class="input-field">
+              <i class="material-icons prefix">lock_open</i>
+              <input id="password-log-in" type="password" minlength="4" class="validate">
+              <label for="password-log-in">Password</label>
+              <span class="helper-text" data-error="invalid password" data-success="perfectly"></span>
+            </div>
+            <a href="#" class="modal-close center log-in-button deep-orange darken-1 waves-effect waves-light btn">Вход</a>
+          </div>
+
+          <div id="registration-swipe" class="center">
+            <div class="input-field">
+              <i class="material-icons prefix">account_circle</i>
+              <input id="user_name" type="text" minlength="4" class="validate">
+              <label for="user_name">Name</label>
+              <span class="helper-text" data-error="invalid name" data-success="perfectly"></span>
+            </div>
+            <div class="input-field">
+              <i class="material-icons prefix">email</i>
+              <input id="email-registration" type="email" class="validate">
+              <label for="email-registration">Email</label>
+              <span class="helper-text" data-error="invalid email" data-success="perfectly"></span>
+            </div>
+            <div class="input-field">
+              <i class="material-icons prefix">lock_open</i>
+              <input id="password-registration" type="password" minlength="4" class="validate">
+              <label for="password-registration">Password</label>
+              <span class="helper-text" data-error="invalid password" data-success="perfectly"></span>
+            </div>
+            <div class="input-field">
+              <i class="material-icons prefix">lock_open</i>
+              <input id="password-registration-confirm" type="password" minlength="4" class="validate">
+              <label for="password-registration-confirm">Confirm Password</label>
+              <span class="helper-text" data-error="invalid password" data-success="perfectly"></span>
+            </div>
+            <a href="#" class="modal-close center register-button deep-orange darken-1 waves-effect waves-light btn">Регистрация</a>
+          </div>
+
+        </div>
+        <div class="modal-footer">
+          <a href="#!" class="modal-close waves-effect waves-green btn-flat modal-close-footer-btn">Закрыть</a>
+        </div>
+      </div>
+
     </header>
 
     <main class="main">

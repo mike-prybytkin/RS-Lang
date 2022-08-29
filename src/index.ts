1c5b6f5758a0bc621b56843f5c66df68e0dca363
@Mike-Prybytkin <prybytkin@bk.ru>
2022-08-29 04:47:09 +0300

    refactor: remove commented code

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,5 @@
 import './style/index.scss';
 import AppController from './components/app/app.controller';
-// import PopUpController from './components/pop-up/pop-up.controller';
 
 const app = new AppController();
 app.initApp();
-
-// const aaa = new PopUpController();
-// const a = aaa.registrationUser({ email: 'Gаgddddd@mail.ru', password: '12345678', name: 'Mikkkke' });
-// a.then((res) => {
-//   if (res) {
-//     console.log('вы зареганы!');
-//     console.log(res);
-//   } else {
-//     console.log('уже существует');
-//   }
-// });
